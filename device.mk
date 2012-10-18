f37bda113356e33d2a40f08bab4483f4b4906711
Bajee11 <bajee11@gmail.com>
2012-10-17 21:32:37 -0400

    Remove overlay and cleanup

--- device.mk
+++ device.mk
@@ -19,8 +19,6 @@ $(call inherit-product-if-exists, vendor/samsung/d2att/d2att-vendor.mk)
 
 
 
-## overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/d2att/overlay
 ## common overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-gsm
 
